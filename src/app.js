cf8e812bf090d33f2d0f61a32d08a48e8f63f9e3
Brandon Graves <bgraves14@gmail.com>
2015-08-19 16:48:11 +0100

    jquery done

--- src/app.js
+++ src/app.js
@@ -8,36 +8,73 @@ DisplayTemp = function() {
   temperature.innerHTML = thermostat.temperature();
 };
 
-var temperature = document.getElementById('temperature'); {
-  temperature.innerHTML = thermostat.temperature();
-};
+$(document).ready(function() {
+  $('#temperature').show(function(){
+    DisplayTemp();
+    TemperatureColour();
+  })
 
-var upButton = document.getElementById('upButton');
-upButton.onclick = function() {
-                                thermostat.increaseTemperature();
-                                DisplayTemp();
-                                TemperatureColour();
-                              };
+  $('#upButton').click(function(){
+    thermostat.increaseTemperature();
+    DisplayTemp();
+    TemperatureColour();
+  })
 
-var downButton = document.getElementById('downButton');
-downButton.onclick = function() {
-                                  thermostat.decreaseTemperature();
-                                  DisplayTemp();
-                                  TemperatureColour();
-                                };
+  $('#downButton').click(function() {
+    thermostat.decreaseTemperature();
+    DisplayTemp();
+    TemperatureColour();
+  })
 
-var resetButton = document.getElementById('resetButton');
-resetButton.onclick = function() {
-                                    thermostat.resetTemperature();
-                                    DisplayTemp();
-                                    TemperatureColour();
-                                  };
+  $('#resetButton').click(function() {
+    thermostat.resetTemperature();
+    DisplayTemp();
+    TemperatureColour();
+  })
 
-var saveButton = document.getElementById('saveButton');
-saveButton.onclick = function() {
-                        console.log('hello');
-                                     thermostat.switchMode();
-                                     TemperatureColour();
-                                     DisplayTemp();
+  $('#saveButton').click(function() {
+    console.log('hello');
+     thermostat.switchMode();
+     TemperatureColour();
+     DisplayTemp();
 
-                                 };
+  })
+
+})
+
+
+
+
+// var temperature = document.getElementById('temperature'); {
+//   temperature.innerHTML = thermostat.temperature();
+// };
+
+// var upButton = document.getElementById('upButton');
+// upButton.onclick = function() {
+//                                 thermostat.increaseTemperature();
+//                                 DisplayTemp();
+//                                 TemperatureColour();
+//                               };
+
+// var downButton = document.getElementById('downButton');
+// downButton.onclick = function() {
+//                                   thermostat.decreaseTemperature();
+//                                   DisplayTemp();
+//                                   TemperatureColour();
+//                                 };
+
+// var resetButton = document.getElementById('resetButton');
+// resetButton.onclick = function() {
+//                                     thermostat.resetTemperature();
+//                                     DisplayTemp();
+//                                     TemperatureColour();
+//                                   };
+
+// var saveButton = document.getElementById('saveButton');
+// saveButton.onclick = function() {
+//                         console.log('hello');
+//                                      thermostat.switchMode();
+//                                      TemperatureColour();
+//                                      DisplayTemp();
+//
+//                                  };
